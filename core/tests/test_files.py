ecc0cd955b4d6279b39137f4d539ca9bc7dfd4de
Marcelo Lerendegui <marcelo@lerendegui.com>
2019-04-26 21:36:35 -0400

    changed imports in core tests. I copyed from server and left the imports as: "from server.something import"

--- core/tests/test_files.py
+++ core/tests/test_files.py
@@ -1,7 +1,7 @@
-from server.files import fio_to_b64s
-from server.files import b64s_to_fio
-from server.files import fio_to_s
-from server.files import s_to_fio
+from core.files import fio_to_b64s
+from core.files import b64s_to_fio
+from core.files import fio_to_s
+from core.files import s_to_fio
 
 import pytest
 
@@ -30,8 +30,8 @@ def test_conversion():
     ]
 )
 def test_s_b_s(in_s):
-    from server.files import s_to_b
-    from server.files import b_to_s
+    from core.files import s_to_b
+    from core.files import b_to_s
 
     b = s_to_b(in_s)
     out_s = b_to_s(b)
@@ -40,8 +40,8 @@ def test_s_b_s(in_s):
 
 
 def test_s_b64s_s():
-    from server.files import s_to_b64s
-    from server.files import b64s_to_s
+    from core.files import s_to_b64s
+    from core.files import b64s_to_s
 
     in_s = 'This is a Message!'
     b64s = s_to_b64s(in_s)
@@ -51,8 +51,8 @@ def test_s_b64s_s():
 
 
 def test_s_fio_s():
-    from server.files import s_to_fio
-    from server.files import fio_to_s
+    from core.files import s_to_fio
+    from core.files import fio_to_s
 
     in_s = 'This is a Message!'
     fio = s_to_fio(in_s)
@@ -62,10 +62,10 @@ def test_s_fio_s():
 
 
 def test_s_b_b64s_b_s():
-    from server.files import s_to_b
-    from server.files import b_to_b64s
-    from server.files import b64s_to_b
-    from server.files import b_to_s
+    from core.files import s_to_b
+    from core.files import b_to_b64s
+    from core.files import b64s_to_b
+    from core.files import b_to_s
 
     in_s = 'This is a Message!'
     b = s_to_b(in_s)
@@ -77,12 +77,12 @@ def test_s_b_b64s_b_s():
 
 
 def test_s_b_b64s_fio_b64s_b_s():
-    from server.files import s_to_b
-    from server.files import b_to_b64s
-    from server.files import b64s_to_fio
-    from server.files import fio_to_b64s
-    from server.files import b64s_to_b
-    from server.files import b_to_s
+    from core.files import s_to_b
+    from core.files import b_to_b64s
+    from core.files import b64s_to_fio
+    from core.files import fio_to_b64s
+    from core.files import b64s_to_b
+    from core.files import b_to_s
 
     in_s = 'This is a Message!'
     b = s_to_b(in_s)
